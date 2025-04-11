c46d30b997d9f1f1a0f97eefa11e3188de2d349d
vladislav_shalimov <vladislav.shalimov03@gmail.com>
2025-04-12 02:05:25 +0400

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,7 @@
-123;
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => <h1>Hello Vite + TS!</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(<App />);
