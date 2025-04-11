af059568fb5eca9744e7d330c5a98bf045f7fcef
vladislav_shalimov <vladislav.shalimov03@gmail.com>
2025-04-11 19:49:28 +0400

    husky test

---
+++ eslint.config.js
@@ -0,0 +1,58 @@
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
+import parser from '@typescript-eslint/parser';
+import eslintPluginTS from '@typescript-eslint/eslint-plugin';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import configPrettier from 'eslint-config-prettier';
+
+export default [
+  {
+    ignores: ['node_modules/**', 'dist/**', 'build/**']
+  },
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      globals: {
+        React: 'readonly'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': eslintPluginTS,
+      react: eslintPluginReact,
+      'react-hooks': eslintPluginReactHooks,
+      prettier: eslintPluginPrettier,
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      },
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json',
+        },
+      },
+    },
+    rules: {
+      // TypeScript
+      '@typescript-eslint/no-unused-vars': 'warn',
+
+      // React
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+
+      // Prettier
+      'prettier/prettier': 'error',
+    }
+  },
+  // Apply Prettier config to disable conflicting ESLint rules
+  configPrettier
+];
